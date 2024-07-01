856f88f0dc80a4987ccd464ce12e5fcc675697cd
Nina <gadgetgeekette@hotmail.co.uk>
2024-07-01 12:32:35 +0100

    Further project setup

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+import styles from "./style-app"
+
+export default function Index() {
+  return (
+    <View>
+      <Text style={styles.default}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
